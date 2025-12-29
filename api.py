a4bdd311aece48ae8a829dc0591b312999098f46
Anshul <yanshul7162005@gmail.com>
2025-12-30 02:26:18 +0530

    Allow CORS from all origins

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],  # allow from anywhere
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
